83f27dac3e7bd9aad09a7be0986965d91bb1bee4
Paulo Rodrigues <80362527+pauloRodOliveira@users.noreply.github.com>
2021-08-31 21:25:05 -0300

    Update let-var-const.js

--- (DS203) DESENVOLVIMENTO PARA INTERNET II/aula3(31-08-2021)/let-var-const.js
+++ (DS203) DESENVOLVIMENTO PARA INTERNET II/aula3(31-08-2021)/let-var-const.js
@@ -4,9 +4,19 @@
    mas se ela for declarada dentro da função, ela vai
    ser somente vista dentro da função.
 */
+function raizQuadrada(){
+    var raiz =  console.log("Digite qq valor = ");
+    console.log(raiz);
+}
 
+/* const 
+   o programa abaixo vai dar erro, pois não é possível mudar o valor de 
+   uma const.
+*/
+
+const valor = 100;
 
 function raizQuadrada(){
-    var raiz = console.read("Digite qq valor = ");
-    console.log
+    valor = 200;
+    console.log(raiz);
 }
