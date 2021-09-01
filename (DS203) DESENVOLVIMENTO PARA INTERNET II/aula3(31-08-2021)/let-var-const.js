91583639653ebda3f7cad86f192d862b9346c73b
Paulo Rodrigues <80362527+pauloRodOliveira@users.noreply.github.com>
2021-08-31 21:12:07 -0300

    input e tipos de variáveis

---
+++ (DS203) DESENVOLVIMENTO PARA INTERNET II/aula3(31-08-2021)/let-var-const.js
@@ -0,0 +1,12 @@
+/* var quer dizer que a variável é local,
+   se eu coloco ela dentro da function com var,
+   ela não vai ser encontrada fora da função.
+   mas se ela for declarada dentro da função, ela vai
+   ser somente vista dentro da função.
+*/
+
+
+function raizQuadrada(){
+    var raiz = console.read("Digite qq valor = ");
+    console.log
+}
