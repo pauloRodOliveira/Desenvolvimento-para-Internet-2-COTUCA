6153c5ded549f91e812cd756f3502580ea18c271
Paulo Rodrigues <80362527+pauloRodOliveira@users.noreply.github.com>
2021-11-16 22:35:28 -0300

    update

--- (DS203) DESENVOLVIMENTO PARA INTERNET II/Consultas do projDESINTIINoturno/criando database, criando tabela clientes e colocando instancias.sql
+++ (DS203) DESENVOLVIMENTO PARA INTERNET II/Consultas do projDESINTIINoturno/criando database, criando tabela clientes e colocando instancias.sql
@@ -1,8 +1,10 @@
-create database projDESINTIINoturno
+--create database projDESINTIINoturno
 
-drop table clientes
+--drop table clientes
 
-CREATE TABLE CLIENTES (
+--create schema DesInt
+
+CREATE TABLE DesInt.CLIENTES (
   idClie int NOT NULL identity(1,1),
   cpfClie varchar(11) NOT NULL,
   nomeClie varchar(40) NOT NULL,
@@ -10,9 +12,9 @@ CREATE TABLE CLIENTES (
   emailClie varchar(60) NOT NULL,
  PRIMARY KEY(idClie));
 
-INSERT INTO CLIENTES VALUES ('11111111111', 'Monica Ferraz',convert(date,'1990-12-04'),'ferraz@ig.com.br');
-INSERT INTO CLIENTES VALUES ('22222222222', 'Beatriz Segal',convert(date, '1990-10-25'),'bsegal@cotuca.br');
-INSERT INTO CLIENTES VALUES ('33333333333','Luciana Silva', convert(date,'2000-03-03'),'luvaz@ig.com.br');
-INSERT INTO CLIENTES VALUES ('66666666666','Anderson Nascimento',convert(date,'1980-08-19'),'nasc@ig.com.br');
+INSERT INTO DesInt.CLIENTES VALUES ('11111111111', 'Monica Ferraz',convert(date,'1990-12-04'),'ferraz@ig.com.br');
+INSERT INTO DesInt.CLIENTES VALUES ('22222222222', 'Beatriz Segal',convert(date, '1990-10-25'),'bsegal@cotuca.br');
+INSERT INTO DesInt.CLIENTES VALUES ('33333333333','Luciana Silva', convert(date,'2000-03-03'),'luvaz@ig.com.br');
+INSERT INTO DesInt.CLIENTES VALUES ('66666666666','Anderson Nascimento',convert(date,'1980-08-19'),'nasc@ig.com.br');
 
-select * from CLIENTES
+select * from DesInt.CLIENTES
