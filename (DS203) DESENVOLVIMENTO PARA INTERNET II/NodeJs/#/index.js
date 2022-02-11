285eebdf22f573052921189af914a38b6cbc4506
Paulo Rodrigues <80362527+pauloRodOliveira@users.noreply.github.com>
2022-02-11 19:13:35 -0300

    update

--- (DS203) DESENVOLVIMENTO PARA INTERNET II/NodeJs/#/index.js
+++ (DS203) DESENVOLVIMENTO PARA INTERNET II/NodeJs/#/index.js
@@ -1,24 +1,42 @@
 const express = require("express");
-const app = express(); 
-const handlebars = require('express-handlebars');
+var app = express(); 
 const Sequelize = require('sequelize');
+const handlebars = require("express-handlebars");
 
 //Config
   //Template Engine
-    app.engine('handlebars', handlebars({defaultLayout:'main'}));
-    app.set('view engine', 'handlebars');
-  //Conexão com o banco de dados
-  const Sequelize = require('sequelize')
-  const sequelize = new Sequelize('sistemadecadastro', 'root', '2910', {
-    host: "localhost",
-    dialect: 'mysql'
-  });
-  sequelize.authenticate().then(function(){
-    console.log("Conectado com sucesso");
-  }).catch(function(erro){
-    console.log("Falha ao se conectar: " + erro);
-  });
+  const hbs = handlebars.create({
+      defaultLayout: 'main'
+  })
+  app.engine('handlebars',() => hbs)
+  app.set('view engine', 'handlebars')
+  app.set('views', './views')
 
-app.listen(3000, function(){
-  console.log("servidor rodando na url http://localhost:3000");
+  //Conexão com o banco de dados
+    const sequelize = new Sequelize('sistemadecadastro', 'root', '2910', {
+      host: "localhost",
+      dialect: 'mysql'
+    });
+    sequelize.authenticate().then(function(){
+      console.log("Conectado com sucesso");
+    }).catch(function(erro){
+      console.log("Falha ao se conectar: " + erro);
+    });
+
+  //Rotas
+    app.get('/cadastro', function(req,res){
+      //res.render(__dirname + "/views/formulario.handlebars")
+      res.render("../views/formulario");
+    });
+
+    app.get('/', function(req,res){
+      //res.render(__dirname + "/views/formulario.handlebars")
+      res.render("../views/formulario");
+    });
+
+const PORTA = 4000
+app.listen(PORTA, function(){
+  console.log(`servidor rodando na url http://localhost:${PORTA}`);
 }); // toda a programação deverá vir antes
+
+
--- (DS203) DESENVOLVIMENTO PARA INTERNET II/NodeJs/Modulos/calc.js
+++
@@ -1,9 +0,0 @@
-var Soma = require("./soma");
-var sub = require("./sub");
-var multi = require("./multi");
-var div = require("./div");
-
-console.log(Soma(2, 7));
-console.log(sub(2, 7));
-console.log(multi(2, 7));
-console.log(div(8, 2));
